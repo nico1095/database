5f4974c35f042b2e5cd396a68917b789601a3c65
nico1095 <agiso@students.kennesaw.edu>
2025-11-15 18:43:22 -0500

    Update sql.py

--- sql.py
+++ sql.py
@@ -1,1 +1,7 @@
-
+# Program Name: sql.py 
+# Course: IT3883/Section W02
+# Student Name: Anthony Giso
+# Assignment Number: 5
+# Due Date: 11/16/ 2025
+# Purpose: python program that calculates the final averages for a group of students and print the results in descending order by grade. 
+# Used Python module and chapters.
